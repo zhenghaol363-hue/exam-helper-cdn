a1b27f90988e2c936c84219e94a5f03d1fe69cf1
zhenghaol363-hue <user@example.com>
2026-08-03 22:25:53 +0800

    init

---
+++ inject-all.js
@@ -0,0 +1,64 @@
+(function(){
+function h(s){var r=0x811c9dc5;for(var i=0;i<s.length;i++){r=((r^s.charCodeAt(i))*0x01000193)>>>0;}return(r>>>0).toString(16).padStart(8,"0");}
+function c(s){return(s||"").replace(/<[^>]+>/g,"").replace(/\s+/g,"").replace(/[（]/g,"(").replace(/[）]/g,")");}
+var B={"f3c8c0d7":"B","03ffd027":"B","5d392e42":"B","819b2262":"C","12ee4992":"B","b5ce862b":"B","7f4c7464":"B","d3190f82":"A","97703fb2":"A","89a45016":"A","4d1b5761":"A","c43e0c7b":"B","589f0d1e":"A","7c6d9efb":"B","fc462094":"B","442283a3":"B","c73a499d":"B","3e4b7e2c":"C","6df8e8e4":"A","d5b0c68c":"A","a6434452":"B","ba5a7883":"B","a0f4abbd":"A","858cb90a":"B","7177205e":"D","a1e3bab0":"D","21a4649d":"D","44092469":"A","bf869f28":"A","cf80d058":"B","93a04183":"A","32cfd187":"A","a1e85aa5":"B","f7bf5e1b":"B","e3c77f9a":"A","38a4acf9":"A","71fdb185":"A","265c023a":"ABC","391a4628":"ABC","9448d79b":"ABC","ad79a500":"ABCD","60db125a":"ABC","7f29a41c":"ABCD","642b10a3":"ABC","5218001b":"ABC","b0b6ece9":"ABCD","d4374b19":"ABCD","9aae327a":"ABCD","098e70f2":"ABCD","f0792959":"ABCD","31a281f8":"ABC","ab4d7583":"ABCD","bdc1126c":"ABC","3c60e2d9":"ABCD","c38f6b72":"ABC","368cd52e":"ABCD","7c7de86f":"ABCD","405f0a15":"ABC","7ad9c501":"ABCD","5439e073":"ABCD","8c3094ab":"ABCD","6a31d60c":"AB","43ecab8a":"ABC","3b9d2bca":"ABC","cb138006":"ABC","b781e636":"ABC","5b816d16":"A","7c9b457a":"A","056027e4":"B","e48c2411":"B","f0a39d0a":"A","d3cbb7dc":"A","6228563e":"B","5cd6df82":"A","365f873d":"B","db494258":"A","981b7e3b":"A","f5d7f450":"B","cf7cb3aa":"A","185d6966":"B","f724b8cd":"A","fa73e872":"B","edd88e42":"B","5fabe01d":"A","3fd9064c":"B","d2fe97ec":"A","ac54a307":"A","4933e4f2":"A","aec5ef06":"B","fdcb1556":"B","9e5a8d27":"B","ad32bead":"A","d7226d1b":"A","2e147a6f":"B","d3ca4b70":"A","89dce226":"A","8a53efe0":"A","fa798cf1":"B","9179c134":"A","2349688f":"C","02e85334":"B","1c626c97":"A","45e998e4":"B","31045d75":"C","ba6e946a":"A","79185afa":"B","ae7dcb15":"C","c0a8cb8a":"A","42b51fe3":"A","acef2770":"A","0dd4fa20":"B","7722a774":"A","9671e04c":"A","d5688237":"A","1d4c703e":"A","e1f60774":"A","e65a9b28":"A","d01df54a":"A","f12159e0":"A","f0cc7719":"B","a3290bd8":"A","31f2216c":"A","a6aa0800":"A","2699cc7d":"A","d8bd2eda":"A","212bcf19":"D","7b8ddc0f":"D","33051c5b":"A","3b86a9ff":"A","4045532e":"A","442c7b6c":"A","5ac296af":"A","128abac9":"A","ab383a34":"A","cb5e4055":"B","14030e9c":"A","198d658f":"B","dc79df9f":"C","1fc12a1d":"D","88af2bd2":"A","5b01b2ae":"A","5defe626":"D","ffd8f69a":"A","9387ddca":"A","648840ca":"A","9a99fd92":"A","dafad371":"A","0e2750f3":"A","83f30a42":"A","5e0ee130":"A","3ce1ddb8":"A","4590444b":"A","3f348d46":"ABCD","07f799e5":"ABCD","fa3311c6":"ABCD","85aafcd5":"ABCD","29e7e6b4":"ABCD","390ac85d":"ABCD","9ca26fc2":"ABC","3276fa14":"ABCD","f83c3085":"ABCD","8eb5b9d6":"ABCD","f1b9cc21":"ABCD","85aced09":"ABCD","7db1f500":"ABCD","a9b5b675":"ABCDE","d811f0a7":"ABCE","7cfadd26":"ABC","f6cafdaf":"ABCDE","5fe9b261":"ABCE","f95af979":"AB","49eda9f2":"ABC","eb3719a8":"ABCD","f03f3263":"ABC","6d5614ac":"ABCD","31d08991":"ABC","a5a834d5":"ABCD","a25a4b78":"ABCD","34f3ee97":"ABCD","1672a121":"ABC","4c948ce8":"ABCE","aecbe252":"A","1effc22e":"A","41436c45":"B","c0f0818b":"B","04ce5449":"A","9c60852e":"B","113596ab":"A","28bea066":"B","bdefab9b":"A","cf192207":"A","5cded8c5":"B","f423ed61":"A","84150eb6":"B","12c2f614":"B","6b7ceb43":"B","cdd22160":"A","8e3a94a1":"A","6e11b05c":"B","f632c391":"B","726f2cdb":"A","60044c70":"A","375198fe":"B","e2011a98":"A","46e17a60":"B","a9c4ee26":"B","487ff251":"A","8eda1d70":"B","539aaf18":"A","6930e332":"A","32dc4a50":"C","e84dbbe7":"A","36a06637":"C","e2c51249":"B","18b17f5d":"A","8d7ef8a4":"B","4b629be7":"B","58b48518":"C","47f1457b":"B","7bd0b8b7":"B","41d4dbfd":"C","e4d86a5f":"C","10fb2d18":"D","2d9ed89b":"C","a6f175f8":"D","e92a0119":"D","480eb65f":"B","311e281b":"C","36d69139":"D","65fd08a0":"B","6f667068":"B","b7079366":"A","2b2b23e3":"D","8a394c18":"A","2fd5493b":"A","469b0980":"D","d22f8b4f":"A","b61ddff1":"A","f78f907e":"A","d1680317":"A","5ced1456":"B","6aa0a141":"D","04e8a9ab":"A","09f20711":"A","2f3d59d7":"C","4cb55408":"D","aede592b":"C","44fd0330":"A","123125ff":"A","d3674b97":"A","30d5ce7c":"D","a4b54e1b":"B","b0020f48":"C","3bb492c3":"D","51388cf4":"A","f67140b0":"D","16940a5b":"D","71712135":"B","d56a2df4":"D","4f21db21":"C","21b8842f":"C","d784293e":"A","0ea5a057":"A","dc503c58":"C","17b1ebdd":"D","ec1a1ed0":"A","03cba8aa":"D","08ea3a0d":"A","c545c46d":"D","22eb182e":"A","adef3ab0":"D","01891523":"ABCD","489c7e55":"ABCD","5922e50c":"ABCD","03fbcc3e":"ABCE","c60712b2":"ABCD","ed028372":"ABCD","679bd1fa":"ABCD","a87701fe":"ABCD","5e62e390":"AB","a0c886bd":"ABCD","929757b5":"ABCD","9e9eb98a":"ABC","beeb556c":"ABC","3e511b58":"ABCD","088461a2":"ABCD","ba12fa84":"ABCD","051be726":"ABC","e0745798":"ABCD","b19df55e":"ABC","eddd49f5":"ABCD","ffac026b":"ABCD","258190ce":"ABCD","429a61e0":"ABCD","7df43cb2":"ABCD","8e5a1d59":"ABCD","cc4a2d88":"ABCD","a1e68b46":"ABCE","cc51e238":"ABCE","0abeeb43":"ABCD","80cf13a2":"ABCD","ccfff5b0":"A","c5d646e6":"B","96674800":"A","e3e6700a":"A","6d580daf":"A","421baf19":"B","22417f99":"A","255f4f18":"B","1a4aab4a":"A","27a39299":"B","2594b076":"A","9d4a3c39":"A","2f4e1aa8":"B","bc0ebf8a":"B","577e98f6":"A","50fb8956":"A","592801ee":"B","f5d43678":"A","a495143d":"A","8f31f121":"B","20bcf4b3":"A","70ca68a9":"B","82c7ccec":"A","19a62da9":"A","b75e3793":"A","e56f9bfd":"A","60db5cc3":"B","db9dadab":"A","a836edce":"A","95d040ef":"B","ce197d83":"C","9a759cfa":"D","da9b50b1":"D","668c579a":"B","2937531f":"D","32145a0d":"D","21f8edbf":"D","171e0ac3":"D","a52cc2ff":"D","cf37d000":"D","6d816eac":"D","8f83a9fa":"D","74f73c30":"A","0d75cc16":"D","1a4ef3d0":"C","71f5bf39":"A","59228346":"B","9c3edb28":"A","eddfc3a7":"D","123d5ecb":"D","e0c32f94":"D","5494a2a1":"D","975c70e0":"D","ba57f48e":"D","6978a91b":"D","07d97320":"B","a0316034":"D","b25af51b":"C","99e401ea":"D","adcbc74e":"D","161578b0":"C","2dc15414":"C","71523629":"D","900ad7d6":"D","c605c182":"ABCDE","2cd69078":"ABCDE","d7d18934":"ABCD","b6e11083":"ABCD","2376e15e":"ABCD","9b5b277d":"ABCDE","6d445820":"ABCD","d1997302":"ABCDE","0cf52077":"ABC","31983be8":"ABCDE","e63530da":"ABCE","0e207abf":"ABCE","8a1e8310":"ABCDE","8515f098":"ABCD","2004d0b0":"ABCD","05a459a6":"ABCDE","747c3e22":"ABCDE","2593daf9":"ABCD","a0056ce6":"ABCD","08ea6905":"ABCDE","5251082d":"ABCD","f5271049":"ABCE","7a3a689b":"ABCD","a517ca62":"ABCD","3158f569":"ABCE","217da407":"AC","51c07257":"ABCD","f44aa827":"ABC","a3c77bb9":"A","b3b8a815":"B","f0760fd4":"A","1a8d441f":"B","422d8c88":"B","e74ed1a3":"A","0660af44":"A","f97d14d4":"B","7e076f58":"A","a15a3a41":"A","fda24bc8":"A","7219045a":"B","9647a7d4":"A","79de503e":"B","0c8abf32":"B","09b7b836":"A","7cbdfec8":"B","3ab24a8c":"A","2d3ed444":"B","7c15fcdc":"B","88ceba1c":"A","d0879235":"B","6607773a":"A","078a1f66":"B","8cd1d98d":"A","65d41204":"B"};
+
+// === 暴力策略：扫描所有可能的题目位置 ===
+var qs=[], seen=new Set();
+
+// 策略1: 找所有包含 radio/checkbox 的父级容器
+document.querySelectorAll("input[type=radio],input[type=checkbox]").forEach(function(inp){
+  var container=inp;
+  for(var i=0;i<8;i++){container=container.parentElement;if(!container)break;
+    if(container.nodeName==="BODY")break;
+    if(seen.has(container))break;
+    var txt=(container.textContent||"").trim();
+    if(txt.length>20&&txt.length<2000){
+      seen.add(container);qs.push(container);break;
+    }
+  }
+});
+
+// 策略2: 如果上面没找到，扫描全部
+if(!qs.length){
+  document.querySelectorAll("div,li,section,fieldset").forEach(function(el){
+    if(seen.has(el))return;
+    var txt=(el.textContent||"").trim();
+    if(txt.length>20&&txt.length<2000&&el.querySelectorAll("input").length>=2){
+      seen.add(el);qs.push(el);
+    }
+  });
+}
+
+if(!qs.length){alert("没找到题目，请确认在考试/练习页面");return;}
+
+// 匹配答案
+var hit=0,miss=0;
+qs.forEach(function(p){
+  // 取题干：找容器内的标题/第一个长文本
+  var stemEl=p.querySelector("h2,h3,h4,h5,.title,[class*=title],[class*=stem],strong,b,p");
+  var stem=c((stemEl||p).textContent||"");
+  // 去掉选项文本，只留题干
+  var inputs=p.querySelectorAll("input");
+  inputs.forEach(function(inp){
+    var label=inp.parentElement;if(label)stem=stem.replace(c(label.textContent||""),"");
+  });
+  
+  var key=h(stem.substr(0,80));
+  var ans=B[key];
+  if(!ans){miss++;return;}
+  hit++;ans=ans.toUpperCase();
+  
+  // 填答案
+  var cbs=p.querySelectorAll("input[type=checkbox]");
+  var rds=p.querySelectorAll("input[type=radio]");
+  if(cbs.length){
+    cbs.forEach(function(cb,i){cb.checked=ans.indexOf(String.fromCharCode(65+i))>=0;cb.dispatchEvent(new Event("change",{bubbles:1}));});
+  }else if(rds.length){
+    var idx=ans.charCodeAt(0)-65;
+    if(idx>=0&&idx<rds.length){rds[idx].checked=true;rds[idx].dispatchEvent(new Event("change",{bubbles:1}));}
+  }
+});
+
+alert("完成！找到"+qs.length+"题，命中"+hit+"，未命中"+miss);
+})();
